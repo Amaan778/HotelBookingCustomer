67f93713e27d977a3045c727ac1b5ba2e90767ff
Amaan Ansari <amaanansari778@gmail.com>
2025-05-20 04:07:34 +0530

    Hotel Booking Customer

--- app/src/main/java/com/app/hotelbookingcustomer/Dashboard.kt
+++ app/src/main/java/com/app/hotelbookingcustomer/Dashboard.kt
@@ -1,20 +1,20 @@
 package com.app.hotelbookingcustomer
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Dashboard : AppCompatActivity() {
+    private lateinit var linear:LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        linear=findViewById(R.id.linear)
+
     }
 }
